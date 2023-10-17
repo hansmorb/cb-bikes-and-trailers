670976c86711e1ef19d8c68521c494d56ba661cc
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-10-17 10:40:52 -0400

    added scaffolding

---
+++ cb-bikes-and-trailers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     CB Bikes And Trailers
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          hansmorb
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     cb-bikes-and-trailers
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Cb_Bikes_And_Trailers
+ */
+
+// Your code starts here.
